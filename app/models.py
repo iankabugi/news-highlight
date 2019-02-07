9f09c7ba2485a8e6c9ee88b8cc2c547b6ac76803
Ian Kabugi <iank299@gmail.com>
2019-02-07 10:09:36 +0300

    add your classes here

--- app/models.py
+++ app/models.py
@@ -1,17 +1,3 @@
-class Source:
-    '''
-    News source class to define news source objects
-    '''
-    def __init__(self, id, name, description, url, category, language, country):
-        self.id = id
-        self.name = name
-        self.description = description
-        self.url = url
-        self.category = category
-        self.language = language
-        self.country = country
-
-
 class Article:
     '''
     News article class to define news article objects
@@ -26,3 +12,19 @@ class Article:
         self.urlToimage = urlToimage
         self.publishedAt = publishedAt
         self.content = content
+
+class Source:
+    '''
+    News source class to define news source objects
+    '''
+    def __init__(self, id, name, description, url, category, language, country):
+        self.id = id
+        self.name = name
+        self.description = description
+        self.url = url
+        self.category = category
+        self.language = language
+        self.country = country
+
+
+
