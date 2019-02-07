d29696a9999ecf49574acc088e8bf745fc4cc9ee
Ian Kabugi <iank299@gmail.com>
2019-02-07 14:29:44 +0300

    add a  second API base URL  to outsource the source of news

--- config.py
+++ config.py
@@ -5,7 +5,9 @@ class Config:
   general configuration class
   '''
   NEWS_API_BASE_URL = 'https://newsapi.org/v2/{}?language=en&apiKey={}'
+
   NEWS_API_KEY = 'b58639b93a564639907c634b2cd74203'
+  
   NEWS_API_BASE_URL2 = 'https://newsapi.org/v2/everything?sources=bbc-news,al-jazeera-english,cnn,independent,google-news,the-telegraph,mashable,the-lad-bible,buzzfeed,bloomberg,engadget,espn,fortune&sortBy=publishedAt&pageSize=100&apiKey={}'
 
 
